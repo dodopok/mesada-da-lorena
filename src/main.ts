84ca18a5586ea57dee30df94322ebca4ffe29989
Douglas Araujo <prestcon.dsilva@terceiros.mag.com.br>
2023-09-11 22:41:33 -0300

    feat: vercel-analytics

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { inject } from '@vercel/analytics';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+inject();
 
 if (environment.production) {
   enableProdMode();
